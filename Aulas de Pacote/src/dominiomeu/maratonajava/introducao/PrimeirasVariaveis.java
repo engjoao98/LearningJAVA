5556c93ab3cf9ec80a47d3c395fdfbbf945911cf
João Vitor Ferreira <eng.ferreirajoao98@gmail.com>
2026-01-14 21:40:17 -0300

    Criando primeiras varáveis

---
+++ Aulas de Pacote/src/dominiomeu/maratonajava/introducao/PrimeirasVariaveis.java
@@ -0,0 +1,18 @@
+package dominiomeu.maratonajava.introducao;
+
+public class PrimeirasVariaveis {
+    static void main() {
+        int ageOfFather = 10;
+        System.out.println(ageOfFather);
+        System.out.println("A idade do pai é de " + ageOfFather);
+        System.out.println("A idade do pai é de " + ageOfFather + " anos");
+    }
+
+}
+
+// Criação de nova classe
+// psvm pela primeira vez
+// tipos primitivos são 8
+// criação de var.
+// imprimir var.
+// imprimir var. + texto
