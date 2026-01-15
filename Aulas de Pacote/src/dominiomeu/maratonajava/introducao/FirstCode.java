a93506506b7b0dab6865eb3852bddae33a580f41
João Vitor Ferreira <eng.ferreirajoao98@gmail.com>
2026-01-14 21:50:02 -0300

    Limpeza de código
    Tradução e remoção de pequenas redundâncias.

---
+++ Aulas de Pacote/src/dominiomeu/maratonajava/introducao/FirstCode.java
@@ -0,0 +1,17 @@
+package dominiomeu.maratonajava.introducao;
+/*
+    Testing functionality
+    comment by lines
+ */
+
+/**
+ * This is a doc test, which is actually more advised by experts
+ */
+
+public class FirstCode {
+    static void main (){
+        System.out.println("Hello IntelliJ !");
+    }
+}
+
+// Testing single-line comment
---
+++ Aulas de Pacote/src/dominiomeu/maratonajava/introducao/FirstVariables.java
@@ -0,0 +1,10 @@
+package dominiomeu.maratonajava.introducao;
+
+public class FirstVariables {
+    static void main() {
+        int ageOfFather = 10;
+        System.out.println(ageOfFather);
+        System.out.println("The father age is " + ageOfFather);
+        System.out.println("The father age is " + ageOfFather + " years old");
+    }
+}
