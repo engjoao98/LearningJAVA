966ca773a3e99540fbce5e21d3f88d3f633c0595
João Vitor Ferreira <eng.ferreirajoao98@gmail.com>
2026-01-14 21:04:46 -0300

    Testando comentários
    testando funcionalidade, mas entendo que não é aconselhável inserir comentários, a não seja os pertinentes.

--- Aulas de Pacote/src/dominiomeu/maratonajava/introducao/PrimeiroCodigo.java
+++ Aulas de Pacote/src/dominiomeu/maratonajava/introducao/PrimeiroCodigo.java
@@ -1,7 +1,17 @@
 package dominiomeu.maratonajava.introducao;
+/*
+    Testando funcionalidade de
+    comentário por linhas
+ */
+
+/**
+ * Já este é um teste de Java.doc, o que é realmente mais aconselhado pelos especialistas
+ */
 
 public class PrimeiroCodigo {
     public static void main (String[] args){
         System.out.println("Olá IntelliJ !");
     }
 }
+
+// Testando cometário por linha única
