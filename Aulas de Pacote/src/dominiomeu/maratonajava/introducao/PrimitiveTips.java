333076b11d89011f2dc5f2a57fbfe200cb2a10a1
João Vitor Ferreira <eng.ferreirajoao98@gmail.com>
2026-01-14 22:55:05 -0300

    Estudo de Tipos Primitivos

---
+++ Aulas de Pacote/src/dominiomeu/maratonajava/introducao/PrimitiveTips.java
@@ -0,0 +1,31 @@
+package dominiomeu.maratonajava.introducao;
+
+public class PrimitiveTips {
+    static void main() {
+
+        int momAge = 10;
+        long bigNumber = 100000;
+        double bigDecimalNumber = 10.2;
+        float decimalNumber = 1;
+        byte minimalLittleNumber = 10;
+        short littleNumber = 10;
+        boolean v = true;
+        boolean f = false;
+        char letter = 65;
+
+
+        System.out.println(momAge + " = int (int + little size)");
+        System.out.println(bigNumber + " = long (int + big size)");
+
+        System.out.println(bigDecimalNumber + " = double (dec + big size)");
+        System.out.println(decimalNumber + " = float (dec + little size)");
+
+        System.out.println(minimalLittleNumber + " = byte (int + little little size)");
+        System.out.println(littleNumber + " = short (int + little size)");
+
+        System.out.println(v + " = boolean v (true)");
+        System.out.println(f + " = boolean f (false)");
+
+        System.out.println(letter + " = char (ASCII 65)");
+    }
+}
